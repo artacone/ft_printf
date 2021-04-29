2c4891ce537aa7c78437586a624d771a344128ee
artacone <artacone@gmail.com>
2021-04-29 22:59:03 +0300

    add strnlen

--- ft_printf/libft/libft.h
+++ ft_printf/libft/libft.h
@@ -55,6 +55,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 
 size_t	ft_strlen(const char *s);
+size_t	ft_strnlen(const char *s, size_t maxlen);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
